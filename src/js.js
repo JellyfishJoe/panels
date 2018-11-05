cc571cd1775e13c5676d4cec3d1d39899d60a410
JellyfishJoe <jellyfishjoe5@gmail.com>
2018-11-05 15:15:12 -0800

    fsda

--- src/js.js
+++ src/js.js
@@ -1,25 +1,58 @@
 var visible = false;
+var loggedIn = false;
 
 $(document).ready(function(){
 
 	$('#logins').hide();
+	$('#panel').hide();
 
-	//console.log($('#logins').is(':visible'));
+	setTimeout(function(){
+		place();
+		console.log('placed');
+	}, 1000);
 
-	$('body:not(#open)').click(function(){
-		console.log('body clicked');
-		if($('#logins').is(':visible')){
-			$('#logins').hide();
+	$('#close').click(function(){
+		$('#panel').fadeOut('fast');
+	});
+
+	$('body:not(#open)').click(function(event){
+		if(event.target.id !== 'open'){
+			if($('#logins').is(':visible')){
+				$('#logins').slideUp();
+				visible = false;
+			}
 		}
-		console.log($('#logins').is(':visible'));
 	});
 
 	$('#open').click(function(){
-		if(visible = !visible){ 
-			$('#logins').slideDown();
-		}else{
-			$('#logins').slideUp();
+		if(loggedIn == false){
+			if(visible = !visible){ 
+				$('#logins').slideDown();
+			}else{
+				$('#logins').slideUp();
+			}
+		} else{
+			$('#open').html('Log In');
+			loggedIn = false;
 		}
 	});
 
+	$('#button').click(function(){
+		$('#logins').slideUp();
+		visible = false;
+		setTimeout(function(){
+			$('#open').html('Logout');
+			loggedIn = true;
+		}, 500);
+	});
 });
+
+function place(){
+	var top = ($(window).height() - $('#panel').height()) / 2;
+	var left = ($(window).width() - $('#panel').width()) / 2;
+	$('#panel').css({
+		'top': top,
+		'left': left,
+		'z-index': 50
+	}).fadeIn('slow');
+}
